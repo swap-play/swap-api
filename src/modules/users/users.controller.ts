34aa8948b07515f269ea4c723a6b9886fcf7d10b
João Victor <j2003.victir@gmail.com>
2024-01-04 10:25:08 -0300

    feat: create user and list all feature

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './dto/user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: UserDto) {
+    return this.usersService.create(userDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+}
---
+++ src/shared/database/repositories/users.repository.ts
@@ -0,0 +1,20 @@
+import { PrismaService } from './../prisma.service';
+import { Injectable } from '@nestjs/common';
+import { type Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createDto: Prisma.UserCreateArgs) {
+    return this.prismaService.user.create(createDto);
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findUnique(findUniqueDto: Prisma.UserFindUniqueArgs) {
+    return this.prismaService.user.findUnique(findUniqueDto);
+  }
+}
