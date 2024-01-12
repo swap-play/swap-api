0ad66140e107af8f588ecab8362a578f8ed2c84e
João Victor <j2003.victir@gmail.com>
2024-01-12 15:13:13 -0300

    feat: add endpoint to get uploaded images

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,11 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
+
+  app.use(
+    '/tmp/uploads',
+    express.static(join(__dirname, '..', 'tmp', 'uploads')),
+  );
 
   await app.listen(3000);
 }
